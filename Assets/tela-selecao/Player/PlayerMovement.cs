dccef703ad25402edbb86583328e14b5f654b3dc
Caio Siqueira da Silva <csiqueirasilva@gmail.com>
2017-08-24 02:38:20 -0300

    Added .gitignore and redid some collisions

---
+++ Assets/tela-selecao/Player/PlayerMovement.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour {
+
+	public Animator anim;
+	public float speed = 4.0f;
+	// Use this for initialization
+	void Start () {
+	
+		anim = GetComponent<Animator> (); //Pegar referencia do boneco
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		float input_x = Input.GetAxisRaw ("Horizontal");
+		float input_y = Input.GetAxisRaw ("Vertical");
+
+
+		bool isWalking = (Mathf.Abs(input_x) + Mathf.Abs(input_y)) > 0;
+
+		anim.SetBool ("Walking",isWalking);
+		if (isWalking) {
+			anim.SetFloat("x",input_x);
+			anim.SetFloat("y",input_y);
+
+			transform.position += new Vector3(input_x,input_y,0).normalized * Time.deltaTime * speed;
+
+		}
+	}
+}
---
+++ Assets/tela-selecao/ScrollTopFix.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+// Script to set scrollbar at top when loading new gameobject
+public class ScrollTopFix : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnEnable() {
+		StartCoroutine(SetScrollTop());
+	}
+
+	public IEnumerator SetScrollTop () {
+		yield return null; // Waiting just one frame is probably good enough, yield return null does that
+		Scrollbar bar = GetComponentInChildren<Scrollbar>();
+		bar.value = 1;
+	}
+
+}
