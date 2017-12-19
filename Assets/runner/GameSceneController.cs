59df2768fe1589c11339aa44f550662cc753347b
Caio Siqueira da Silva <csiqueirasilva@gmail.com>
2017-12-19 11:25:57 -0200

    Updated runner to hide floor sprite (so it uses background image floor patterns)

--- Assets/runner/GameSceneController.cs
+++ Assets/runner/GameSceneController.cs
@@ -38,8 +38,9 @@ public class GameSceneController : MonoBehaviour {
 			//Gerando o chão
 			if (player != null && gamePointer < player.transform.position.x + safeSpawningArea) {
 				GameObject blockObject = Instantiate (floorPrefab);
-				blockObject.transform.SetParent (this.transform);
-				Block block = blockObject.GetComponent<Block> ();
+                blockObject.transform.SetParent (this.transform);
+                blockObject.GetComponentInChildren<SpriteRenderer>().enabled = false;
+                Block block = blockObject.GetComponent<Block> ();
 				blockObject.transform.position = new Vector3 (gamePointer + block.size / 2, -3.21f, 11);
 				gamePointer += block.size;
 				Debug.Log(block.size);
