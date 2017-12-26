c08a2400c22f214fc2ec94640ffd2b2ceb0a5d54
Caio Siqueira da Silva <csiqueirasilva@gmail.com>
2017-12-26 18:58:28 -0200

    Added credits screen

--- Assets/LevelManager.cs
+++ Assets/LevelManager.cs
@@ -4,15 +4,30 @@ using UnityEngine.SceneManagement;
 
 public class LevelManager : MonoBehaviour {
 
+    public Canvas canvas;
+
+    public void Start()
+    {
+        exibirTelaInicial();
+    }
+
 	public void loadNextLevel() {
         int idx = (SceneManager.GetActiveScene().buildIndex + 1) % SceneManager.sceneCountInBuildSettings;
         SceneManager.LoadScene(idx);
 	}
 
-	public void quitRequest(){
-		Debug.Log ("Quit requested.");
-		Application.Quit();
-	}
+	public void exibirCreditos(){
+        canvas.transform.Find("credits").gameObject.SetActive(true);
+        canvas.transform.Find("startScreen").gameObject.SetActive(false);
+    }
+
+    public void exibirTelaInicial()
+    {
+        if(canvas != null) {
+            canvas.transform.Find("credits").gameObject.SetActive(false);
+            canvas.transform.Find("startScreen").gameObject.SetActive(true);
+        }
+    }
 }
 
 
